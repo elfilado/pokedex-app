1c851b82539d231f0a9041716780a5af4facf2ef
elfilado <dorian.elfilali@gmail.com>
2022-06-28 15:44:01 +0200

    reorganize modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
-import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { path: 'pokemons', component: ListPokemonComponent },
-  { path: 'pokemons/:id', component: DetailPokemonComponent },
   { path: '', redirectTo: 'pokemons', pathMatch: 'full' },
   //intercepte toutes les routes (dangereux, donc on le met tout en bas)
   //nb: le routeur d'angular lit les routes du haut vers le bas
--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
     selector: 'app-page-not-found',
     template: `
     <div class='center'>
-      <img src="http://assets.pokemon.com/assets/cms2/img/pokedex/full/035.png"/>
+      <img src="http://assets.pokemon.com/assets/cms2/img/pokedex/full/122.png"/>
       <h1>Hey, cette page n'existe pas !</h1>
       <a routerLink="/pokemons" class="waves-effect waves-teal btn-flat">
         Retourner à l'accueil
