f48762aa6dc348a26b0f70dfe7068dd2a2e1e9f5
elfilado <dorian.elfilali@gmail.com>
2022-07-08 19:10:14 +0200

    simulate server

--- src/app/pokemon/pokemon-form/pokemon-form.component.ts
+++ src/app/pokemon/pokemon-form/pokemon-form.component.ts
@@ -13,11 +13,13 @@ export class PokemonFormComponent implements OnInit {
 
   @Input() pokemon: Pokemon;
   types: string[];
+  isAddForm: boolean;
 
   constructor(private pokemonService: PokemonService, private router: Router) { }
 
   ngOnInit(): void {
     this.types = this.pokemonService.getPokemonTypeList();
+    this.isAddForm = this.router.url.includes('add');
   }
 
   hasType(type: string): boolean {
@@ -37,8 +39,14 @@ export class PokemonFormComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log("Formulaire soumis.");
-    this.router.navigate(['/pokemons', this.pokemon.id]);
+    if(this.isAddForm) {
+      this.pokemonService.addPokemon(this.pokemon)
+        .subscribe((pokemon: Pokemon) =>  this.router.navigate(['/pokemons', pokemon.id]));
+    }
+    else {
+      this.pokemonService.updatePokemon(this.pokemon)
+        .subscribe(() =>  this.router.navigate(['/pokemons', this.pokemon.id]));
+    }
   }
 
   isTypesValid(type: string): boolean {
--- src/app/pokemon/pokemon.ts
+++ src/app/pokemon/pokemon.ts
@@ -1,9 +1,28 @@
 export class Pokemon{
+
     id: number;
     hp: number;
     attack: string;
     name: string;
     picture: string;
-    types: Array<string>;
+    types: string[];
     created: Date;
+
+    constructor( 
+       // id: number = 0,
+        hp: number = 100,
+        attack: string = "Charge",
+        name: string = "Mon nom",
+        picture: string = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/xxx.png",
+        types: string[] = ['Normal'],
+        created: Date = new Date()
+    ){
+        //this.id = id;
+        this.hp = hp;
+        this.attack = attack;
+        this.name =  name;
+        this.picture = picture;
+        this.types = types;
+        this.created = created;
+    }
 }
